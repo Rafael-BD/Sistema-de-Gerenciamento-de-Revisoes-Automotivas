2ac3d9d318a456dbfa3c573af03c09ef57e1ebe3
Rafael Bettini Dias <67149165+Rafael-BD@users.noreply.github.com>
2021-11-24 16:05:21 -0300

    arquivos

---
+++ ProjetoWeb/Models/Cliente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWeb.Models
+{
+    [Table("RevisoesDeCarro")]
+    public class Cliente
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        int _id;
+        public int Id
+        {
+            get { return _id; }
+
+            set { _id = value; }
+        }
+
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+
+            set { _nome = value; }
+        }
+
+        [Display(Name = "Telefone")]
+        [Column("Telefone")]
+        int _telefone;
+        public int Telefone
+        {
+            get { return _telefone; }
+
+            set { _telefone = value; }
+        }
+
+
+        [Display(Name = "Endereco")]
+        [Column("Endereco")]
+        string _endereco;
+        public string Endereco
+        {
+            get { return _endereco; }
+
+            set { _endereco = value; }
+        }
+
+        [Display(Name = "CPF")]
+        [Column("CPF")]
+        int _cpf;
+        public int CPF
+        {
+            get { return _cpf; }
+
+            set { _cpf = value; }
+        }
+    }
+}
---
+++ ProjetoWeb/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWeb.Models
+{
+    public class Veiculo
+    {
+        public string NumeroPlaca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public float Valor { get; set; }
+    }
+}
